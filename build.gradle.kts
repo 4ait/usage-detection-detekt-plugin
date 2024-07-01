eeb637b78b69a729126f20c6c5b5ed11bc3bab4f
Evgeniy Simonenko <tiikara93@gmail.com>
2024-07-01 12:33:54 +1000

    Update developer details in build.gradle.kts
    Expanded the developer details in build.gradle.kts file. Added organization name, role and more to the developer data. Also, adjusted the copyright to match the organization name for consistency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,18 @@ publishing {
             id = "tikara"
             name = "Evgeniy Simonenko"
             email = "tiikara93@gmail.com"
+            organization.set("4A LLC")
+            roles.set(
+              listOf(
+                "Software Developer",
+                "Head of Development"
+              )
+            )
           }
+        }
+        organization {
+          name = "4A LLC"
+          url = "https://4ait.ru"
         }
         scm {
           connection = "scm:git:git://github.com:4ait/usage-detection-detekt-plugin.git"
@@ -82,7 +93,7 @@ publishing {
 
 jreleaser {
   project {
-    copyright.set("Company 4A")
+    copyright.set("4A LLC")
   }
   gitRootSearch.set(true)
   signing {
